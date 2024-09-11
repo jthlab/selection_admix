8ad63865a09f5b76876fdd09ce0eaa4346755dc5
Jonathan Terhorst <terhorst@gmail.com>
2024-09-11 16:17:29 -0400

    add simulation code

--- src/bmws/sim.py
+++ src/bmws/sim.py
@@ -1,5 +1,7 @@
 import logging
 
+import jax.numpy as jnp
+
 from bmws.betamix import Dataset, forward
 
 logger = logging.getLogger(__name__)
@@ -68,7 +70,7 @@ def sim_and_fit(
     Ne_fit=None,  # Ne to use for estimation (if different to that used for simulation)
     em_iterations=3,  # use empirical bayes to infer prior hyperparameters
     M=100,  # number of mixture components
-    **kwargs
+    **kwargs,
 ):
     # this is now just a frontend for sim_admix
     T = len(mdl["s"]) + 1
@@ -87,94 +89,73 @@ def sim_and_fit(
 
 def sim_admix(
     mdl: dict,
+    data: Dataset,
     seed: int,
-    thetas,
-    samples,
-    Ne=1e4,  # effective population size
-    Ne_fit=None,  # Ne to use for estimation (if different to that used for simulation)
     em_iterations=3,
     M=100,
-    estimate_kwargs=None,
+    estimate_kwargs={"alpha": 0.0, "beta": 0.0, "gamma": 0.0},
 ):
     """
     Simulate from Wright-Fisher model for several populations, sample admixed individulas,
     and perform inference on resulting dataset.
 
     Params:
-        mdl: A dictionary containing the entries "s", "h", and "f0", detailing the selection coefficients, dominance
-             parameters, and initial frequencies. The leading dimensions of each of theses should be K, the number of
-             populations, and the trailing dimension of "s" and "h" should be T - 1, the number of time points minus one.
+        mdl: A dictionary containing the entries "s" and "f0", detailing the selection coefficients
+             initial frequencies. The leading dimensions of each of theses should be K, the number of
+             populations, and the trailing dimension of "s" should be T - 1, the number of time points minus one.
+        data: A Dataset which conveys the sampling model and admixture proportions. data.obs is ignored.
         seed: random seed
-        thetas: array of dimension [T, N, K], giving the admixture proportions of each individual at each time point.
-        samples: array of dimension [T, N], giving the number of samples collected from each individual at each time point.
-                 for example, if only haploid data are collected, then every entry of :samples: should be one; if diploid
-                 genotypes are collected, the should be 2. setting samples=0 denotes missing data.
-        Ne: effective population size. Either a number, or an array of shape [T - 1, K] giving the effective population
-            size for each population between each time point. If a number, the same Ne is used for all populations and
-            times.
         Ne_fit: Ne parameter used for fitting, if different from that used for simulation. Same semantics as Ne.
         estimate_kwargs: arguments passed to :estimate: when model fitting.
     """
-    if estimate_kwargs is None:
-        estimate_kwargs = {"lam": 1.0}
     # Parameters
-    assert thetas.ndim == 3
-    T, N, K = thetas.shape
-    assert samples.ndim == 2
-    assert samples.shape == (T, N)
+    T = data.T
+    K = mdl["s"].shape[1]
     assert mdl["s"].ndim == 2
-    assert mdl["s"].shape == (T - 1, K)
+    assert mdl["s"].shape == (T, K)
+    Ne = mdl["Ne"]
     if isinstance(Ne, float):
         Ne = np.full_like(mdl["s"], Ne)
+    h = np.full_like(mdl["s"], 0.5)
     assert Ne.ndim == 2
-    assert Ne.shape == (T - 1, K)
+    assert Ne.shape == (T, K)
 
-    # Set up population size.
-    if not Ne_fit:
-        Ne_fit = Ne
-    else:
-        Ne_fit = np.array(Ne_fit)
-        assert Ne_fit.shape == Ne.shape
+    Ne_fit = Ne
 
     # Simulate true trajectory
     rng = np.random.default_rng(seed)
     # simulate the wright-fisher model. all parameter vectors are reversed so that times runs from past to present.
     s = mdl["s"]
-    h = mdl["h"]
     f0 = mdl["f0"]
     afs = [
         sim_wf(Ne_i[::-1], s_i[::-1], h_i[::-1], f0_i, rng)[::-1]
         for Ne_i, s_i, h_i, f0_i in zip(Ne.T, s.T, h.T, f0.T)
     ]
     afs = np.transpose(afs)
-    obs = np.zeros([T, N, 2], dtype=int)
-    for t in range(T):
-        for n in range(N):
-            k = rng.choice(K, p=thetas[t, n])
-            p = afs[t, k]
-            a = samples[t, n]
-            d = rng.binomial(a, p)
-            obs[t, n] = [a, d]
-    data, nzi = Dataset(thetas=thetas, obs=obs).resort()
+    assert afs.shape == (T + 1, K)
+    obs = []
+    for t, theta, (n, _) in zip(*data):
+        k = rng.choice(K, p=theta)
+        p = afs[t, k]
+        d = rng.binomial(n, p)
+        obs.append((int(n), d))
+    sim_data = data._replace(obs=jnp.array(obs))
 
     # setup prior
-    s = np.zeros([T - 1, data.K])
+    s = np.zeros([T, data.K])
     ab = np.ones([2, data.K]) + 1e-4
     for i in range(em_iterations):
         logger.info("EM iteration %d", i)
-        ab, prior = empirical_bayes(ab0=ab, s=s, data=data, nzi=nzi, Ne=Ne, M=M)
+        ab, prior = empirical_bayes(ab0=ab, s=s, data=sim_data, Ne=Ne, M=M)
         logger.info("ab: %s", ab)
-        s = estimate(data=data, Ne=Ne_fit, prior=prior, nzi=nzi, **estimate_kwargs)
+        s = estimate(data=sim_data, Ne=Ne_fit, prior=prior, **estimate_kwargs)
         logger.info("s: %s", s)
-
-    betas, _ = forward(s, Ne, data, nzi, prior)
 
     return {
         "s_hat": s,
-        "betas": betas,
+        "sim": sim_data,
         "obs": obs,
         "Ne": Ne,
         "true_af": afs,
         "prior": prior,
-        "data": data,
     }
